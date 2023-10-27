6612ecb4cc35b875d1562c78cd425d8334b78466
raphaelloner <raphael.loner@gmail.com>
2023-10-27 14:09:49 +0200

    add projekt created by spring initalizr

---
+++ matomogenerator/src/main/java/com/challenge/matomogenerator/MatomogeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.matomogenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatomogeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatomogeneratorApplication.class, args);
+	}
+
+}
---
+++ matomogenerator/src/test/java/com/challenge/matomogenerator/MatomogeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.matomogenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatomogeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
