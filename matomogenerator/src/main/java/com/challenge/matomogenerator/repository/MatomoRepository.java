c3f15cf860ab3cf7d43a77956fb058697cf02e53
raphaelloner <raphael.loner@gmail.com>
2023-10-27 18:54:59 +0200

    implemented MatomoRepository

---
+++ matomogenerator/src/main/java/com/challenge/matomogenerator/repository/MatomoRepository.java
@@ -0,0 +1,8 @@
+package com.challenge.matomogenerator.repository;
+
+import com.challenge.matomogenerator.data.Matomo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatomoRepository extends JpaRepository<Matomo,Long> {
+
+}
