6437d546bddac56afbe2516a624faa152a9d8ea1
raphaelloner <raphael.loner@gmail.com>
2023-10-27 18:39:57 +0200

    data class matomo

--- matomogenerator/src/main/java/com/challenge/matomogenerator/data/Matomo.java
+++ matomogenerator/src/main/java/com/challenge/matomogenerator/data/Matomo.java
@@ -1,5 +1,60 @@
 package com.challenge.matomogenerator.data;
 
-public class Matomo {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "matomo")
+public class Matomo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "matomo_id")
+    private Long id;
+    @Column(name="matomo_namespace")
+    private String namespace;
+    @Column(name = "matomo_name")
+    private String name;
+    @Column(name = "matomo_host")
+    private String host;
+
+    public Matomo(Long id, String namespace, String name, String host) {
+        this.id = id;
+        this.namespace = namespace;
+        this.name = name;
+        this.host = host;
+    }
+
+    public Matomo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
 }
